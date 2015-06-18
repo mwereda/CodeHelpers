487c764d10148ad1151cc76e55f63615ff217970
Wereda, Michal <mwereda@future-processing.com>
2015-06-18 10:31:52 +0200

    ActionResolverAttribute
    This attribute can be used to select action method that should execute
    when e.g. there are two actions with the same name in controller. Action
    is selected on the basis of request parameters.

---
+++ CodeHelpers/MVC/ActionFilters/ActionResolverAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+
+namespace CodeHelpers.MVC.ActionFilters
+{
+	public class ActionResolverAttribute : ActionMethodSelectorAttribute
+	{
+		private readonly string[] parameters;
+
+		public ActionResolverAttribute(params string[] parameterNames)
+		{
+			this.parameters = parameterNames;
+		}
+
+		public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
+		{
+			foreach (var param in this.parameters)
+			{
+				if (controllerContext.HttpContext.Request[param] == null)
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
