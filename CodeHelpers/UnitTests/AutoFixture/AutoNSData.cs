8d4054b0647aaa32007a11423135f76d7c63bc4f
Michal Wereda <mwereda@future-processing.com>
2015-07-02 07:37:29 +0200

    AutoNSData attribute for unit tests
    AutoNSData attribute for unit tests with AutoFixture, NSubstitute and
    xUnit. Used to inject test doubles and SUT to unit test method.

---
+++ CodeHelpers/UnitTests/AutoFixture/AutoNSData.cs
@@ -0,0 +1,14 @@
+﻿using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoNSubstitute;
+using Ploeh.AutoFixture.Xunit;
+
+namespace CodeHelpers.UnitTests.AutoFixture
+{
+    public class AutoNSData : AutoDataAttribute
+    {
+        public AutoNSData()
+            : base(new Fixture().Customize(new AutoNSubstituteCustomization()))
+        {
+        }
+    }
+}
