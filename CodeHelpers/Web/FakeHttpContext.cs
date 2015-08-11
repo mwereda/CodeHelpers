19b63d58fada1f0d0d561d5e8b59e25a8b809bc7
Wereda, Michal <mwereda@future-processing.com>
2015-08-11 08:24:45 +0200

    HttpContext and HttpRequest for unit testing

---
+++ CodeHelpers/Web/FakeHttpContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+
+namespace CodeHelpers.Web
+{
+	public class FakeHttpContext : HttpContextBase
+	{
+		public FakeHttpContext()
+		{
+			RequestBase = new FakeHttpRequest();
+		}
+
+		public FakeHttpRequest RequestBase { get; set; }
+		public override HttpRequestBase Request { get { return RequestBase; } }
+	}
+}
---
+++ CodeHelpers/Web/FakeHttpRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Web;
+
+namespace CodeHelpers.Web
+{
+	public class FakeHttpRequest : HttpRequestBase
+	{
+		public Uri UrlBase { get; set; }
+		public override Uri Url { get { return UrlBase; } }
+		public override HttpCookieCollection Cookies { get { return new HttpCookieCollection(); } }
+		public override NameValueCollection Form { get { return new NameValueCollection(); } }
+	}
+}
