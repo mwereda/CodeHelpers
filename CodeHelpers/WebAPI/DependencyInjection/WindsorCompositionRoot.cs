aceabe84da86c6b20ae4a8be8070102f171467d7
Michal Wereda <mi.wereda@gmail.com>
2016-01-01 12:05:27 +0100

    WebAPI Casle.Windsor composition root.

---
+++ CodeHelpers/WebAPI/DependencyInjection/WindsorCompositionRoot.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dispatcher;
+using Castle.Windsor;
+
+namespace CodeHelpers.WebAPI.DependencyInjection
+{
+    /// <summary>
+    /// Castle.Windsor composition root for WebAPI. 
+    /// To use it put this line into Global.asax.cs: GlobalConfiguration.Configuration.Services.Replace(typeof(IHttpControllerActivator), new WindsorCompositionRoot(container));
+    /// </summary>
+    public class WindsorCompositionRoot : IHttpControllerActivator
+    {
+        private readonly IWindsorContainer container;
+
+        public WindsorCompositionRoot(IWindsorContainer container)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException("container");
+            }
+
+            this.container = container;
+        }
+
+        public IHttpController Create(
+            HttpRequestMessage request,
+            HttpControllerDescriptor controllerDescriptor,
+            Type controllerType)
+        {
+            var controller = (IHttpController)this.container.Resolve(controllerType);
+            request.RegisterForDispose(new Release(() => this.container.Release(controller)));
+
+            return controller;
+        }
+
+        private class Release : IDisposable
+        {
+            private readonly Action release;
+
+            public Release(Action release)
+            {
+                this.release = release;
+            }
+
+            public void Dispose()
+            {
+                this.release();
+            }
+        }
+    }
+}
