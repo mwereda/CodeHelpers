8ab39d3459ec3203373b7cd6dcd3f952abe92956
Michal Wereda <mi.wereda@gmail.com>
2016-01-10 13:16:06 +0100

    Castle.Windsor extensions registering MVC controllers and ApiControllers.

---
+++ CodeHelpers/MVC/DependencyInjection/ContainerExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web.Mvc;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+
+namespace CodeHelpers.MVC.DependencyInjection
+{
+    public static class ContainerExtensions
+    {
+        public static void RegisterControllers(this IWindsorContainer container)
+        {
+            var controllers = GetControllers(Assembly.GetExecutingAssembly());
+            foreach (var controller in controllers)
+            {
+                container.Register(Component.For(controller).LifestylePerWebRequest());
+            }
+        }
+
+        public static void RegisterControllers(this IWindsorContainer container, Assembly assembly)
+        {
+            var controllers = GetControllers(assembly);
+            foreach (var controller in controllers)
+            {
+                container.Register(Component.For(controller).LifestylePerWebRequest());
+            }
+        }
+        
+        private static IList<Type> GetControllers(Assembly assembly)
+        {
+            var controllers = assembly.GetTypes().Where(x => x.BaseType == typeof(Controller)).ToList();
+
+            return controllers;
+        }
+    }
+}
---
+++ CodeHelpers/WebAPI/DependencyInjection/ContainerExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web.Http;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+
+namespace CodeHelpers.WebAPI.DependencyInjection
+{
+    public static class ContainerExtensions
+    {
+        public static void RegisterApiControllers(this IWindsorContainer container)
+        {
+            var controllers = GetControllers(Assembly.GetExecutingAssembly());
+            foreach (var controller in controllers)
+            {
+                container.Register(Component.For(controller).LifestylePerWebRequest());
+            }
+        }
+
+        public static void RegisterApiControllers(this IWindsorContainer container, Assembly assembly)
+        {
+            var controllers = GetControllers(assembly);
+            foreach (var controller in controllers)
+            {
+                container.Register(Component.For(controller).LifestylePerWebRequest());
+            }
+        }
+
+        private static IList<Type> GetControllers(Assembly assembly)
+        {
+            var controllers = assembly.GetTypes().Where(x => x.BaseType == typeof(ApiController)).ToList();
+
+            return controllers;
+        }
+    }
+}
